7e2eeb7c7ce77fd793b7eddd2d70a138389d04d4
Autakou <37272124+Autakou@users.noreply.github.com>
2018-03-11 18:23:35 +0100

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+var prefix = ("&")
+
+bot.on('ready', function() {
+    bot.user.setGame("se toucher avec mon Maître");
+    console.log("Connected");
+});
+
+bot.login("NDIyMzYzNzMwOTMyNzkzMzQ0.DYas2A.DWgMeJ5BINdAe4TPG8krDN9j2iE");
+
+bot.on('message', message => {
+    if (message.content === prefix + "help")
+        message.author.createDM().then(channel => {
+            message.channel.sendMessage("Vous avez reçu un message privé :) !")
+            channel.send("Liste des commandes : \n -help \n -allo")
+
+        }
+    )
+})
+
+bot.on('message', message => {
+    if (message.content === prefix + "allo")
+    message.channel.sendMessage("Je suis là Maître !")
+})
